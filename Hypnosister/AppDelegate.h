cf2417ca6376ad8895b4d8582085428c0ff59ac5
Glenn Tu <glenn.tu@gmail.com>
2014-12-30 22:48:32 -0800

    Initial Commit

---
+++ Hypnosister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by Glenn on 12/30/14.
+//  Copyright (c) 2014 Glenn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
