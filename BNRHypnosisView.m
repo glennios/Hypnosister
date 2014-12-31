a2964a6d87f7764d480d552890aa3db75cd1465d
Glenn Tu <glenn.tu@gmail.com>
2014-12-31 00:21:09 -0800

    draw a circle

--- BNRHypnosisView.m
+++ BNRHypnosisView.m
@@ -10,12 +10,23 @@
 
 @implementation BNRHypnosisView
 
-/*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
-    // Drawing code
+    CGRect bounds = self.bounds; // get your own view's bounds
+    float radius  = MIN(bounds.size.width, bounds.size.height) / 2.0;
+
+    // determine center coord
+    CGPoint center;
+    center.x      = bounds.origin.x + bounds.size.width  / 2.0;
+    center.y      = bounds.origin.y + bounds.size.height / 2.0;
+
+    // render pathstroke
+    UIBezierPath *path = [[UIBezierPath alloc] init];
+    path.lineWidth = 10;
+    [path addArcWithCenter:center radius:radius startAngle:0 endAngle:(2.0*M_PI) clockwise:YES];
+    [[UIColor lightGrayColor] setStroke];
+    [path stroke];
 }
-*/
 
 @end
